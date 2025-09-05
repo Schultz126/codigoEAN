dac0e760c4016e30abdb383cfab032eaab3384a3
Schultz126 <rafael.drzevieski@gmail.com>
2025-09-05 11:12:08 -0300

    Load Factor
    Criado o fator de balanceamento, mas ainda não existe uma função de rehash

--- paisesEAN/hashPaisesEAN.c
+++ paisesEAN/hashPaisesEAN.c
@@ -8,11 +8,11 @@
 #include "PAISES_EANreader.c"
 
 #define HASH 37
-#define TABLE_SIZE 50
-#define INSERT_MAX 25
+#define INSERT_MAX 30
 #define ALL_COUNTRIES 534
-#define DEBUG  
-
+#define INITIAL_TABLE_SIZE 50
+//#define DEBUG  
+int TABLE_SIZE = INITIAL_TABLE_SIZE;
 int comparacoes = 0;
 
 void red() {
@@ -30,7 +30,13 @@ typedef struct {
     int *next;
 }HashNode;
 
-HashNode *hashTable[TABLE_SIZE];
+typedef struct {
+    HashNode *table[INITIAL_TABLE_SIZE];
+    int numberOfElements;
+    float loadFactor;
+}HashTable;
+
+HashTable *hashTable;
 
 unsigned int hash(int code) {
     return code % HASH;
@@ -42,7 +48,7 @@ static inline int hashDuplo(Country* country) {
 
 bool hashInitialize() {
     for(int i = 0; i < TABLE_SIZE; i++) {
-        hashTable[i] = NULL;
+        hashTable->table[i] = NULL;
     }
 }
 
@@ -51,36 +57,37 @@ bool hashInsert(Country *country) {
         return false;
     }
     int index = hash(country->EANcode);  
-    if(hashTable[index] != NULL) {
+    if(hashTable->table[index] != NULL) {
         
         // Endereçamento aberto: procura próxima posição livre
         int originalIndex = index;
         int i = 1;
         do {
             index = (originalIndex + (i*i)) % TABLE_SIZE;
-            if (hashTable[index] == NULL) {
-            hashTable[index] = (HashNode*)malloc(sizeof(HashNode));
-            if (hashTable[index] == NULL) {
+            if (hashTable->table[index] == NULL) {
+            hashTable->table[index] = (HashNode*)malloc(sizeof(HashNode));
+            if (hashTable->table[index] == NULL) {
                 return false;
             }
-            hashTable[index]->country = *country;
-            hashTable[index]->next = NULL;
+            hashTable->table[index]->country = *country;
+            hashTable->table[index]->next = NULL;
+            hashTable->numberOfElements++;
+            hashTable->loadFactor = ((float)(hashTable->numberOfElements) / (TABLE_SIZE));
             return true;
             }
             i++;
         } while (index != originalIndex && i < TABLE_SIZE);
         // Tabela cheia
         return false;
-        return false;
-        
-
     }
-    hashTable[index] = (HashNode*)malloc(sizeof(HashNode));
-    if (hashTable[index] == NULL) {
+    hashTable->table[index] = (HashNode*)malloc(sizeof(HashNode));
+    if (hashTable->table[index] == NULL) {
         return false;
     }
-    hashTable[index]->country = *country;
-    hashTable[index]->next = NULL;
+    hashTable->table[index]->country = *country;
+    hashTable->table[index]->next = NULL;
+    hashTable->numberOfElements++;
+    hashTable->loadFactor = ((float)(hashTable->numberOfElements) / (TABLE_SIZE));
     return true;
 }
 
@@ -89,8 +96,8 @@ int hashSearch(int EANcode) {
     int originalIndex = index;
     int i = 0;
     while (i < TABLE_SIZE) {
-        if (hashTable[index] != NULL) {
-            if (hashTable[index]->country.EANcode == EANcode) {
+        if (hashTable->table[index] != NULL) {
+            if (hashTable->table[index]->country.EANcode == EANcode) {
                 comparacoes++;
                 return index;
             }
@@ -113,7 +120,9 @@ bool deleteFromHashTable(HashNode *toDelete) {
         return false;
     }
     free(toDelete);
-    hashTable[index] = NULL;
+    hashTable->table[index] = NULL;
+    hashTable->numberOfElements--;
+    hashTable->loadFactor = ((float)hashTable->numberOfElements / TABLE_SIZE);
     return true;
     
 }
@@ -121,12 +130,13 @@ bool deleteFromHashTable(HashNode *toDelete) {
 void printTable() {
     printf("Início\n");
     for(int i = 0; i < TABLE_SIZE; i++) {
-        if(hashTable[i] == NULL) {
+        if(hashTable->table[i] == NULL) {
             printf("---> NULL\n");
         } else {
-            printf("---> Código: %d, Nome: %s\n",hashTable[i]->country.EANcode, hashTable[i]->country.country);
+            printf("---> Código: %d, Nome: %s\n",hashTable->table[i]->country.EANcode, hashTable->table[i]->country.country);
         }
     }
+    printf("Fator de carga: %.3f\n", hashTable->loadFactor);
     printf("Fim");
 }
 
@@ -134,27 +144,34 @@ int main() {
     int inseridos = 0, naoInseridos = 0;
     Country countries[ALL_COUNTRIES], toBeInserted[INSERT_MAX];
     readerPaisesEAN(countries);
+
+    // Allocate memory for the hashTable struct
+    hashTable = (HashTable*)malloc(sizeof(HashTable));
+    if (hashTable == NULL) {
+        // Handle allocation error
+        perror("Failed to allocate memory for hash table");
+        return 1;
+    }
+    hashTable->numberOfElements = 0;
     hashInitialize();
 
     srand(time(NULL));
     for(int i = 0; i < INSERT_MAX; i++) {
         toBeInserted[i] = countries[rand() % ALL_COUNTRIES];
     }
-    #ifdef DEBUG
-        for(int i = 0; i < INSERT_MAX; i++) {
-            if(hashInsert(&toBeInserted[i])) {
-                green();
-                printf("Código: %d, Nome: %s Inserido com sucesso\n",toBeInserted[i].EANcode, toBeInserted[i].country);
-                inseridos++;
-                reset(); 
-            } else {
-                red();
-                printf("Código: %d, Nome: %s Não foi inserido\n",toBeInserted[i].EANcode, toBeInserted[i].country);
-                naoInseridos++;
-                reset();
-            }
+    for(int i = 0; i < INSERT_MAX; i++) {
+        if(hashInsert(&toBeInserted[i])) {
+            green();
+            printf("Código: %d, Nome: %s Inserido com sucesso\n",toBeInserted[i].EANcode, toBeInserted[i].country);
+            inseridos++;
+            reset(); 
+        } else {
+            red();
+            printf("Código: %d, Nome: %s Não foi inserido\n",toBeInserted[i].EANcode, toBeInserted[i].country);
+            naoInseridos++;
+            reset();
         }
-    #endif
+    }
     
     printTable();
 
@@ -163,14 +180,14 @@ int main() {
     if(searched == -1) {
         printf("\nCódigo 790 não encontrado\n");
     } else {
-        printf("\nCódigo: %d, Nome: %s\n",hashTable[searched]->country.EANcode, hashTable[searched]->country.country);
+        printf("\nCódigo: %d, Nome: %s\n",hashTable->table[searched]->country.EANcode, hashTable->table[searched]->country.country);
     }
     printf("Comparações feitas durante a busca: %d\n",comparacoes);
     printf("Foram inseridos: %d países e %d ficaram de fora\n", inseridos, naoInseridos);
 
     printf("Nó deletado: %d %s\n",toBeInserted[10].EANcode, toBeInserted[10].country);
     int indexToDelete = hashSearch(toBeInserted[10].EANcode);
-    deleteFromHashTable(hashTable[indexToDelete]);
+    deleteFromHashTable(hashTable->table[indexToDelete]);
     printTable();
 }
 
